0cfab52bcf1395a7d0e41c85d70e21b76734a0a4
unknown <ferdinandimeka20@gmail.com>
2024-02-20 20:32:02 +0100

    added product and user route

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ import path from 'path';
 
 import db from './db/index.js';
 import cookieParser from 'cookie-parser';
+import productRoute from './routes/productRoute.js';
+import userRoute from './routes/userRoute.js';
 
 const port = process.env.PORT || 5001;
 dotenv.config();
@@ -13,6 +15,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
+
+app.use('/api/v1/products', productRoute);
+app.use('/api/v1/users', userRoute);
 
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, '/uploads')));
