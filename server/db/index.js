42d08ac2267f317bcc4f1eb180df104411030cef
unknown <ferdinandimeka20@gmail.com>
2024-02-20 20:27:48 +0100

    bug fix

--- server/db/index.js
+++ server/db/index.js
@@ -1,30 +1,13 @@
-import { MongoClient, ServerApiVersion } from 'mongodb';
+import mongoose from 'mongoose';
 
-const db = () => {
-    const uri = process.env.MONGO_URI;
-
-    // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-    const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
-    });
-
-    async function run() {
+const db = async() => {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log('Connected to MongoDB Successfully 👍')
+    } catch (error) {
+        console.error('Error:', error.message)
+        process.exit(1)
     }
-    }
-    run().catch(console.dir);
 }
 
 export default db;
